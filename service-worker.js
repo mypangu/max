0a36cf6c003d1202fd1cf04a55b875e17c51bd8c
MrPangu <235312546+mypangu@users.noreply.github.com>
2025-10-06 21:07:51 +0530

    0610251100

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "0610251000"; // ✅ Increment this on each update
+const CACHE_NAME = "0610251100"; // ✅ Increment this on each update
 const ASSETS_TO_CACHE = [
   "/",
   "/index.html",
